2e6a27b6d5d6c7afc96e948f5b49de534b07c67d
Peterii <petteri.sallstrom@tuni.fi>
2020-07-04 18:25:15 +0300

    first commit

---
+++ build/precache-manifest.5b4a53e98b0dab69f64a30754b516006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9136e8fb1a29041cb38f225708118a31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d74dbd0f6d4840bbc08",
+    "url": "/static/css/main.7f189203.chunk.css"
+  },
+  {
+    "revision": "506241b702e134ef8865",
+    "url": "/static/js/2.0b9e7a5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b9e7a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d74dbd0f6d4840bbc08",
+    "url": "/static/js/main.bf991634.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express"),
+  app = express(),
+  morgan = require("morgan"),
+  cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
+morgan.token("data", (req, res) => {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+
+let persons = [
+  {
+    id: 1,
+    number: "12-43-245325",
+    name: "Dan Makarooni",
+  },
+  {
+    id: 2,
+    number: "65-43-253425",
+    name: "Jorma Vuori",
+  },
+  {
+    id: 3,
+    number: "5435",
+    name: "The Stone",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>${new Date()}<p>`
+  );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(204).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+  if (
+    person.number === undefined ||
+    person.number.length <= 0 ||
+    person.name === undefined ||
+    person.name.length <= 0 ||
+    persons.filter((p) => p.name === person.name).length !== 0
+  ) {
+    return res.status(400).json({
+      error: "Some data was missing or the name has been taken.",
+    });
+  } else {
+    person.id = Math.floor(Math.random() * 999999);
+    persons = persons.concat(person);
+    res.json(person);
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
